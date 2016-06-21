ce80a141dbf990131ff49e9eac11bfea323af1be
Remco Visser <rvremco@hotmail.com>
2016-06-21 14:41:30 +0200

    First version of location

--- Question1.cs
+++ Question1.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
 using System.Text;
 using Android.App;
 using Android.Content;
+using System.Threading.Tasks;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Locations;
+using Android.Util;
 
 namespace Testapplicatie
 {
 	[Activity(Label = "@string/v1")]
-	public class Question1 : Activity
+	public class Question1 : Activity, ILocationListener
 	{
 		Router router = new Router();
+		static readonly string TAG = "X:" + typeof(Question1).Name;
+		TextView _addressText;
+		Location _currentLocation;
+		LocationManager _locationManager;
+
+		string _locationProvider;
+		TextView _locationText;
+
+		public async void OnLocationChanged(Location location)
+		{
+			_currentLocation = location;
+			if (_currentLocation == null)
+			{
+				_locationText.Text = "Unable to determine your location. Try again in a short while.";
+			}
+			else
+			{
+				_locationText.Text = string.Format("{0:f6},{1:f6}", _currentLocation.Latitude, _currentLocation.Longitude);
+				Address address = await ReverseGeocodeCurrentLocation();
+				DisplayAddress(address);
+
+			}
+		}
+
+		public void OnProviderDisabled(string provider) { }
+
+		public void OnProviderEnabled(string provider) { }
+
+		public void OnStatusChanged(string provider, Availability status, Bundle extras)
+		{
+			Log.Debug(TAG, "{0}, {1}", provider, status);
+		}
 
 		protected override void OnCreate(Bundle bundle)
 		{
@@ -23,14 +59,88 @@ namespace Testapplicatie
 			SetContentView(Resource.Layout.Question_One);
 
 			// Button & eventhandler.
-			Button returnButton = FindViewById<Button>(Resource.Id.returnButton);
-			returnButton.Click += delegate
+		
+
+
+			_addressText = FindViewById<TextView>(Resource.Id.address_text);
+			_locationText = FindViewById<TextView>(Resource.Id.location_text);
+			FindViewById<TextView>(Resource.Id.get_address_button).Click += AddressButton_OnClick;
+
+			InitializeLocationManager();
+		}
+		void InitializeLocationManager()
+		{
+			_locationManager = (LocationManager)GetSystemService(LocationService);
+			Criteria criteriaForLocationService = new Criteria
 			{
-				// Swap to the right activity.
-				StartActivity(typeof(MainActivity));
-				// Close the current layout.
-				Finish();
+				Accuracy = Accuracy.Fine
 			};
+			IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+
+			if (acceptableLocationProviders.Any())
+			{
+				_locationProvider = acceptableLocationProviders.First();
+			}
+			else
+			{
+				_locationProvider = string.Empty;
+			}
+			Log.Debug(TAG, "Using " + _locationProvider + ".");
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			_locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+			Log.Debug(TAG, "Listening for location updates using " + _locationProvider + ".");
+		}
+
+		protected override void OnPause()
+		{
+			base.OnPause();
+			_locationManager.RemoveUpdates(this);
+			Log.Debug(TAG, "No longer listening for location updates.");
+		}
+
+		async void AddressButton_OnClick(object sender, EventArgs eventArgs)
+		{
+			if (_currentLocation == null)
+			{
+				_addressText.Text = "Can't determine the current address. Try again in a few minutes.";
+				return;
+			}
+
+			Address address = await ReverseGeocodeCurrentLocation();
+			DisplayAddress(address);
+		}
+
+		async Task<Address> ReverseGeocodeCurrentLocation()
+		{
+			Geocoder geocoder = new Geocoder(this);
+			IList<Address> addressList =
+				await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+
+			Address address = addressList.FirstOrDefault();
+			return address;
+		}
+
+		void DisplayAddress(Address address)
+		{
+			System.Console.WriteLine(address);
+			if (address != null)
+			{
+				StringBuilder deviceAddress = new StringBuilder();
+				for (int i = 0; i < address.MaxAddressLineIndex; i++)
+				{
+					deviceAddress.AppendLine(address.GetAddressLine(i));
+				}
+				// Remove the last comma from the end of the address.
+				_addressText.Text = deviceAddress.ToString();
+			}
+			else
+			{
+				_addressText.Text = "Unable to determine the address. Try again in a few minutes.";
+			}
 		}
 	}
 }
