4f2bc93f70490a014d7c4940878c68de089bf82a
Maarten de Graaf <maarten@webtraders.nl>
2016-06-21 13:43:08 +0200

    Clean install

--- BicycleInfo/Properties/AssemblyInfo.cs
+++ BicycleInfo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using Android.App;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Testapplicatie")]
+[assembly: AssemblyTitle("BicycleInfo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("jase")]
+[assembly: AssemblyCopyright("${AuthorCopyright}")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- README.md
+++
@@ -1,1 +0,0 @@
-# HR-Project-4
