09ca43037421ab4bfff35151220c3b33c1024624
Jason Sjambar <realjase@gmail.com>
2016-06-21 13:18:38 +0200

    Agenda item can be added by clicking on a button. The following gets added on submit: - Event starts with the current date & time. - Event ends 24 hours after the event start time.

---
+++ Classes/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Java.Util;
+
+namespace Testapplicatie
+{
+	public class Helpers
+	{
+		public Helpers()
+		{
+		}
+
+		public static long convertToMilliseconds(int yr, int month, int day, int hr, int min)
+		{
+			Calendar c = Calendar.GetInstance(Java.Util.TimeZone.Default);
+
+			c.Set(Calendar.DayOfMonth, day);
+			c.Set(Calendar.HourOfDay, hr);
+			c.Set(Calendar.Minute, min);
+			c.Set(Calendar.Month, month);
+			c.Set(Calendar.Year, yr);
+
+			return c.TimeInMillis;
+		}
+	}
+}
+
