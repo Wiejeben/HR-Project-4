44fca94d19b944069940f9c30e6d493fe65cc2dd
Dilan Leeuwendaal <dilan95@hotmail.nl>
2016-06-22 10:27:53 +0200

    OxyPlot has been added to the app. Question 5 contains the plot.

--- Question5.cs
+++ Question5.cs
@@ -11,6 +11,11 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.Xamarin.Android;
+
 namespace Testapplicatie
 {
 	[Activity(Label = "@string/v5")]
@@ -18,22 +23,55 @@ namespace Testapplicatie
 	{
 		Router router = new Router();
 
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			// Set layout view.
-			SetContentView(Resource.Layout.Question_One);
+        private PlotModel CreatePlotModel()
+        {
 
-			// Button & eventhandler.
-			Button returnButton = FindViewById<Button>(Resource.Id.returnButton);
-			returnButton.Click += delegate
-			{
-				// Swap to the right activity.
-				StartActivity(typeof(MainActivity));
-				// Close the current layout.
-				Finish();
-			};
-		}
-	}
+            var plotModel = new PlotModel { Title = "OxyPlot Demo" };
+
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
+
+            var series1 = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.White
+            };
+
+            series1.Points.Add(new DataPoint(0.0, 6.0));
+            series1.Points.Add(new DataPoint(1.4, 2.1));
+            series1.Points.Add(new DataPoint(2.0, 4.2));
+            series1.Points.Add(new DataPoint(3.3, 2.3));
+            series1.Points.Add(new DataPoint(4.7, 7.4));
+            series1.Points.Add(new DataPoint(6.0, 6.2));
+            series1.Points.Add(new DataPoint(8.9, 8.9));
+
+            plotModel.Series.Add(series1);
+
+            return plotModel;
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set layout view.
+            SetContentView(Resource.Layout.Question_One);
+
+            // Button & eventhandler.
+            Button returnButton = FindViewById<Button>(Resource.Id.returnButton);
+            returnButton.Click += delegate
+            {
+                // Swap to the right activity.
+                StartActivity(typeof(MainActivity));
+                // Close the current layout.
+                Finish();
+            };
+
+            var plotView = new PlotView(this);
+            plotView.Model = CreatePlotModel();
+
+            this.AddContentView(plotView,
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent));
+        }
+    }
 }
 
