6c46e182c150fa20d88b9996fdfcfa5ad476b704
Maarten de Graaf <wiejeben@gmail.com>
2016-06-24 10:49:24 +0200

    Make insertion script modular

--- ImportBicycleInfo/Models/BikeTheft.cs
+++ ImportBicycleInfo/Models/BikeTheft.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ImportBicycleInfo
 {
-    class BikeTheft
+    class BikeTheft : Insertable
     {
         public string ID;
         public string Color;
@@ -17,8 +17,6 @@ namespace ImportBicycleInfo
         public DateTime DateTime;
         public string District;
         public string Street;
-
-        private SQLiteConnection Connection;
 
         public BikeTheft(string ID, string district, string street, string brand, string color, DateTime dateTime)
         {
@@ -60,68 +58,13 @@ namespace ImportBicycleInfo
             return new BikeTheft(ID, district, street, brand, color, dateTime);
         }
 
-        private int GetLastInsertedID(string table)
-        {
-            string select_query = "SELECT `seq` FROM `sqlite_sequence` WHERE `name` == @table;";
-            SQLiteCommand command = new SQLiteCommand(select_query, this.Connection);
-            command.Parameters.AddWithValue("@table", table);
-
-            SQLiteDataReader reader = command.ExecuteReader();
-            reader.Read();
-
-            return reader.GetInt32(0);
-        }
-
-        private int GetOrSet(string table, string name, string child_table = null, string child_name = null)
-        {
-            string select_query = "SELECT `id` FROM `" + table + "` WHERE `name` == @name;";
-            using (SQLiteCommand command = new SQLiteCommand(select_query, this.Connection))
-            {
-                command.Parameters.AddWithValue("@name", name);
-
-                SQLiteDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    Console.WriteLine(name);
-                    return reader.GetInt32(0);
-                }
-            }
-            
-            string insert_query = "INSERT INTO `" + table + "` VALUES(null, @name);";
-
-            // Add child ID
-            if (child_table != null)
-            {
-                insert_query = "INSERT INTO `" + table + "` VALUES(null, @name, @child_id);";
-            }
-
-            using (SQLiteCommand command = new SQLiteCommand(insert_query, this.Connection))
-            {
-                command.Parameters.AddWithValue("@name", name);
-
-                // Apply child ID
-                if (child_table != null)
-                {
-                    command.Parameters.AddWithValue("@child_id", this.GetOrSet(child_table, child_name));
-                }
-
-                command.ExecuteNonQuery();
-
-                Console.WriteLine("Already exists");
-                return this.GetLastInsertedID(table);
-            }
-
-            // Fallback
-            return 0;
-        }
-
-        public bool InsertDB(SQLiteConnection connection)
+        public override bool InsertDB(SQLiteConnection connection)
         {
             this.Connection = connection;
             // TODO: Implement create or get
             int brandID = this.GetOrSet("brands", this.Brand);
             int colorID = this.GetOrSet("colors", this.Color);
-            int streetID = this.GetOrSet("streets", this.Street, "districts", this.District);
+            int streetID = this.GetOrSet("streets", this.Street, "districts", "district_id", this.District);
 
             string query = "INSERT INTO `bikethefts` VALUES(@id, @brand_id, @color_id, @street_id);";
             using (SQLiteCommand command = new SQLiteCommand(query, connection))
@@ -131,7 +74,6 @@ namespace ImportBicycleInfo
                 command.Parameters.AddWithValue("@brand_id", brandID);
                 command.Parameters.AddWithValue("@color_id", colorID);
                 command.Parameters.AddWithValue("@street_id", streetID);
-
                 command.ExecuteNonQuery();
             }
 
---
+++ ImportBicycleInfo/Models/Insertable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace ImportBicycleInfo
+{
+    abstract class Insertable
+    {
+        protected SQLiteConnection Connection;
+
+        protected int GetLastInsertedID(string table)
+        {
+            string select_query = "SELECT `seq` FROM `sqlite_sequence` WHERE `name` == @table;";
+            SQLiteCommand command = new SQLiteCommand(select_query, this.Connection);
+            command.Parameters.AddWithValue("@table", table);
+
+            SQLiteDataReader reader = command.ExecuteReader();
+            reader.Read();
+
+            return reader.GetInt32(0);
+        }
+
+        protected int GetOrSet(string table, string name)
+        {
+            string select_query = "SELECT `id` FROM `" + table + "` WHERE `name` == @name;";
+            using (SQLiteCommand command = new SQLiteCommand(select_query, this.Connection))
+            {
+                command.Parameters.AddWithValue("@name", name);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Get existing: " + name);
+                    return reader.GetInt32(0);
+                }
+            }
+
+            string insert_query = "INSERT INTO `" + table + "` VALUES(null, @name);";
+            using (SQLiteCommand command = new SQLiteCommand(insert_query, this.Connection))
+            {
+                command.Parameters.AddWithValue("@name", name);
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Create new: " + name);
+                return this.GetLastInsertedID(table);
+            }
+        }
+
+        protected int GetOrSet(string table, string name, string childTable, string childColumn, string childName)
+        {
+            string select_query = "SELECT `id` FROM `" + table + "` WHERE `name` == @name AND `" + childColumn + "` == @child_name;";
+            using (SQLiteCommand command = new SQLiteCommand(select_query, this.Connection))
+            {
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@child_name", childName);
+
+                SQLiteDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Console.WriteLine("Get existing: " + name + " - With child: " + childName);
+                    return reader.GetInt32(0);
+                }
+            }
+
+            string insert_query = "INSERT INTO `" + table + "` VALUES(null, @name, @child_id);";
+
+            using (SQLiteCommand command = new SQLiteCommand(insert_query, this.Connection))
+            {
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@child_id", this.GetOrSet(childTable, childName));
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Create new: " + name + " - With child: " + childName);
+                return this.GetLastInsertedID(table);
+            }
+        }
+
+        public abstract bool InsertDB(SQLiteConnection connection);
+    }
+}
