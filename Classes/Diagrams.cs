e6048a631b8c87910aad1735eca2d82406edf00d
Jason Sjambar <realjase@gmail.com>
2016-06-24 12:11:16 +0200

    Moved the functions around to fit the class diagram.

--- Classes/Diagrams.cs
+++ Classes/Diagrams.cs
@@ -31,55 +31,6 @@ namespace Testapplicatie
 				LegendOrientation = LegendOrientation.Horizontal,
 				LegendBorderThickness = 0
 			};
-		} 
-
-		public PlotModel createTwoBarModel(List<string> yAxisLabels, string xAxisBar1Name, List<int> xAxisBar1, string xAxisBar2Name, List<int> xAxisBar2)
-		{
-			// BEGIN: MODEL::BASE
-
-			// Axises & their properties
-			var yAxis = new CategoryAxis { Title = yAxisLabel, Position = AxisPosition.Left, IsZoomEnabled = false, IsPanEnabled = false };
-			var xAxis = new LinearAxis { Title = xAxisLabel, Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
-
-			// Bars & their properties
-			var firstBar = new BarSeries { Title = xAxisBar1Name, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
-			var secondBar = new BarSeries { Title = xAxisBar2Name, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
-
-			// END: MODEL::BASE
-
-			// BEGIN: MODEL::VALUES
-
-			// Add Y-AXIS labels
-			foreach (string yAxisL in yAxisLabels)
-			{
-				yAxis.Labels.Add(yAxisL);
-			}
-
-			foreach (var xVal1 in xAxisBar1)
-			{
-				firstBar.Items.Add(new BarItem { Value = xVal1 });
-			}
-
-			foreach (var xVal2 in xAxisBar2)
-			{
-				secondBar.Items.Add(new BarItem { Value = xVal2 });
-			}
-
-			// END: MODEL::VALUES
-
-			// BEGIN MODEL::VISUALIZATION
-
-			// Add axises to model
-			model.Axes.Add(yAxis);
-			model.Axes.Add(xAxis);
-
-			// Add bars to model
-			model.Series.Add(firstBar);
-			model.Series.Add(secondBar);
-
-			// END MODEL::VISUALIZATION
-
-			return model;
 		}
 
 		public PlotModel CreateBarModel(List<string> neighbourhoods, List<int> thefts)
@@ -129,6 +80,55 @@ namespace Testapplicatie
 			model.Axes.Add(xAxis);
 
 			// END: MODEL::VISUALIZATION
+
+			return model;
+		}
+
+		public PlotModel createTwoBarModel(List<string> yAxisLabels, string xAxisBar1Name, List<int> xAxisBar1, string xAxisBar2Name, List<int> xAxisBar2)
+		{
+			// BEGIN: MODEL::BASE
+
+			// Axises & their properties
+			var yAxis = new CategoryAxis { Title = yAxisLabel, Position = AxisPosition.Left, IsZoomEnabled = false, IsPanEnabled = false };
+			var xAxis = new LinearAxis { Title = xAxisLabel, Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+
+			// Bars & their properties
+			var firstBar = new BarSeries { Title = xAxisBar1Name, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+			var secondBar = new BarSeries { Title = xAxisBar2Name, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+
+			// END: MODEL::BASE
+
+			// BEGIN: MODEL::VALUES
+
+			// Add Y-AXIS labels
+			foreach (string yAxisL in yAxisLabels)
+			{
+				yAxis.Labels.Add(yAxisL);
+			}
+
+			foreach (var xVal1 in xAxisBar1)
+			{
+				firstBar.Items.Add(new BarItem { Value = xVal1 });
+			}
+
+			foreach (var xVal2 in xAxisBar2)
+			{
+				secondBar.Items.Add(new BarItem { Value = xVal2 });
+			}
+
+			// END: MODEL::VALUES
+
+			// BEGIN MODEL::VISUALIZATION
+
+			// Add axises to model
+			model.Axes.Add(yAxis);
+			model.Axes.Add(xAxis);
+
+			// Add bars to model
+			model.Series.Add(firstBar);
+			model.Series.Add(secondBar);
+
+			// END MODEL::VISUALIZATION
 
 			return model;
 		}
