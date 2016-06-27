7ff2cb53105605bb140d5ea74680c72f041c568a
Jason Sjambar <realjase@gmail.com>
2016-06-27 14:34:36 +0200

    + Datepicker template + Datepicker instead of a random date added in the agenda ~ Changed the convert to milliseconds function to have base values for hr/min + Another string in the xml.

--- Question6.cs
+++ Question6.cs
@@ -1,67 +1,142 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-
-using Android;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Provider;
-
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
+using Android.Gms.Location;
+using Android.Gms.Common;
+using Android.Gms.Common.Apis;
+using Android.Util;
+using Android.Locations;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using System.Collections.Generic;
 
 namespace Testapplicatie
 {
 	[Activity(Label = "@string/us_6")]
-	public class Question6 : Activity
+	public class Question6 : Activity, GoogleApiClient.IConnectionCallbacks,
+		GoogleApiClient.IOnConnectionFailedListener, Android.Gms.Location.ILocationListener
 	{
-		public int cDay 	= 	DateTime.Now.Day;
-		public int cMonth 	= 	(DateTime.Now.Month - 1);
-		public int cYear 	= 	DateTime.Now.Year;
-		public int cHour 	= 	DateTime.Now.Hour;
-		public int cMin 	= 	DateTime.Now.Minute;
+		GoogleApiClient apiClient;
+		LocationRequest locRequest;
+		Address address;
+		string streetName;
 
-		Router router = new Router();
+		public async void GetLocation()
+		{
+			apiClient.Connect();
+
+			// This method is called when we connect to the LocationClient. We can start location updated directly form
+			// here if desired, or we can do it in a lifecycle method, as shown above 
+
+			// You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
+			Log.Info("LocationClient", "Now connected to client");
+			if (apiClient.IsConnected)
+			{
+				// pass in a location request and LocationListener
+				await LocationServices.FusedLocationApi.RequestLocationUpdates(apiClient, locRequest, this);
+				// In OnLocationChanged (below), we will make calls to update the UI
+				// with the new location data
+			}
+			else
+			{
+				Log.Info("LocationClient", "Please wait for Client to connect");
+			}
+		}
+
+		////Interface methods
+		public void OnConnected(Bundle bundle)
+		{
+			// This method is called when we connect to the LocationClient. We can start location updated directly form
+			// here if desired, or we can do it in a lifecycle method, as shown above 
+
+			// You must implement this to implement the IGooglePlayServicesClientConnectionCallbacks Interface
+			Log.Info("LocationClient", "Now connected to client");
+			GetLocation();
+		}
+
+		public void OnConnectionFailed(ConnectionResult bundle)
+		{
+			// This method is used to handle connection issues with the Google Play Services Client (LocationClient). 
+			// You can check if the connection has a resolution (bundle.HasResolution) and attempt to resolve it
+
+			// You must implement this to implement the IGooglePlayServicesClientOnConnectionFailedListener Interface
+			Log.Info("LocationClient", "Connection failed, attempting to reach google play services");
+		}
+
+		public void OnConnectionSuspended(int i) { }
+
+		public async void OnLocationChanged(Location location)
+		{
+			// This method display changes in the user's location if they've been requested
+
+			// You must implement this to implement the Android.Gms.Locations.ILocationListener Interface
+			Log.Debug("LocationClient", "Location updated");
+
+			address = await LocationInformation.ReverseGeocodeCurrentLocation(this, location);
+			streetName = address.GetAddressLine(0).ToString();
+		}
 
 		protected override void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
 
 			// Set layout view.
-			// SetContentView(Resource.Layout.Question_One);
+			SetContentView(Resource.Layout.Datepicker);
 
-			// Class that can save content for applications.
-			ContentValues eventValues = new ContentValues();
+			if (GooglePlayService.IsGooglePlayServicesInstalled(this))
+			{
+				apiClient = new GoogleApiClient.Builder(this, this, this).AddApi(LocationServices.API).Build();
+				// generate a location request that we will pass into a call for location updates
+				locRequest = new LocationRequest();
+				GetLocation();
+			}
+			else {
+				Log.Error("OnCreate", "Google Play Services is not installed");
+				Toast.MakeText(this, "Google Play Services is not installed", ToastLength.Long).Show();
+				Finish();
+			}
 
-			// We think the standard id for a calendar is 1 on a device.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.CalendarId, 1);
-			// Title for the agenda item.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.Title, "Fiets ophalen.");
-			// Description for the agenda item.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.Description, "Anders word je fiets gejat!");
-			// Location for the agenda item.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.EventLocation, "Somewhere..");
-			// Convert to milliseconds so we can define a start date
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.Dtstart, Helpers.convertToMilliseconds(cYear, cMonth, cDay, cHour, cMin));
-			// Convert to milliseconds so we can define a end date.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.Dtend, Helpers.convertToMilliseconds(cYear, cMonth, cDay+1, cHour, cMin));
+			DatePicker datePicker = (DatePicker)FindViewById(Resource.Id.datePicker);
+			Button confirmButton = FindViewById<Button>(Resource.Id.datePickerSelect);
 
-			// Define the timezones.
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.EventTimezone, "Europe/Berlin");
-			eventValues.Put(CalendarContract.Events.InterfaceConsts.EventEndTimezone, "Europe/Berlin");
+			confirmButton.Click += delegate
+			{
+				int cDay = datePicker.DayOfMonth;
+				int cMonth = datePicker.Month;
+				int cYear = datePicker.Year;
 
-			// Insert the data into the agenda content.
-			var uri = ContentResolver.Insert(CalendarContract.Events.ContentUri, eventValues);
+				// Class that can save content for applications.
+				ContentValues eventValues = new ContentValues();
 
-			// Let's return to the main activity.
-			StartActivity(typeof(MainActivity));
+				// We think the standard id for a calendar is 1 on a device.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.CalendarId, 1);
+				// Title for the agenda item.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.Title, "Fiets ophalen.");
+				// Description for the agenda item.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.Description, "Anders word je fiets gejat!");
+				// Location for the agenda item.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.EventLocation, streetName);
+				// Convert to milliseconds so we can define a start date
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.Dtstart, Helpers.convertToMilliseconds(cYear, cMonth, cDay));
+				// Convert to milliseconds so we can define a end date.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.Dtend, Helpers.convertToMilliseconds(cYear, cMonth, cDay, 23, 59));
+
+				// Define the timezones.
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.EventTimezone, "Europe/Berlin");
+				eventValues.Put(CalendarContract.Events.InterfaceConsts.EventEndTimezone, "Europe/Berlin");
+
+				// Insert the data into the agenda content.
+				var uri = ContentResolver.Insert(CalendarContract.Events.ContentUri, eventValues);
+
+				Toast.MakeText(this, "Uw reminder is toevoegd!", ToastLength.Long).Show();
+
+			};
 
 			// Button & eventhandler.
-			/* Button returnButton = FindViewById<Button>(Resource.Id.returnButton);
+			Button returnButton = FindViewById<Button>(Resource.Id.returnButton);
 			returnButton.Click += delegate
 			{
 				// Swap to the right activity.
@@ -69,7 +144,7 @@ namespace Testapplicatie
 				// Close the current layout.
 				Finish();
 			};
-			*/
+
 		}
 	}
 }
