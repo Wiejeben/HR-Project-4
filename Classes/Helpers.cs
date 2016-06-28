da79c6c3e1323e78e36e33abe0fccae675a2505c
Jason Sjambar <realjase@gmail.com>
2016-06-28 11:55:18 +0200

    Clarifying more values..

--- Classes/Helpers.cs
+++ Classes/Helpers.cs
@@ -11,15 +11,15 @@ namespace Testapplicatie
 
 		public static long convertToMilliseconds(int yr, int month, int day, int hr = 0, int min = 0)
 		{
-			Calendar c = Calendar.GetInstance(Java.Util.TimeZone.Default);
+			Calendar calendar = Calendar.GetInstance(Java.Util.TimeZone.Default);
 
-			c.Set(Calendar.Year, yr);
-			c.Set(Calendar.Month, month);
-			c.Set(Calendar.DayOfMonth, day);
-			c.Set(Calendar.HourOfDay, hr);
-			c.Set(Calendar.Minute, min);
+			calendar.Set(Calendar.Year, yr);
+			calendar.Set(Calendar.Month, month);
+			calendar.Set(Calendar.DayOfMonth, day);
+			calendar.Set(Calendar.HourOfDay, hr);
+			calendar.Set(Calendar.Minute, min);
 
-			return c.TimeInMillis;
+			return calendar.TimeInMillis;
 		}
 	}
 }
