40c776ee541779ec71027d371226eaf8e3cad5ee
Maarten de Graaf <maarten@webtraders.nl>
2016-06-29 15:42:18 +0200

    Implement Path combine

--- AndroidBicycleInfo/Services/Database.cs
+++ AndroidBicycleInfo/Services/Database.cs
@@ -7,21 +7,21 @@ namespace AndroidBicycleInfo
 	public class Database
 	{
 		private static string FileName = "Database.sqlite";
-		private static string Path = Android.OS.Environment.ExternalStorageDirectory.ToString() + "/" + FileName;
+		private static string DbPath = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.ToString(), FileName);
 
 		public static SQLiteConnection Load()
 		{
-			return new SQLiteConnection(Path);
+			return new SQLiteConnection(DbPath);
 		}
 
 		public static void Boot(Activity activity)
 		{
 			// Check if your DB has already been extracted.
-			if (!File.Exists(Path))
+			if (!File.Exists(DbPath))
 			{
 				using (BinaryReader br = new BinaryReader(activity.Assets.Open(FileName)))
 				{
-					using (BinaryWriter bw = new BinaryWriter(new FileStream(Path, FileMode.Create)))
+					using (BinaryWriter bw = new BinaryWriter(new FileStream(DbPath, FileMode.Create)))
 					{
 						byte[] buffer = new byte[2048];
 						int len = 0;
