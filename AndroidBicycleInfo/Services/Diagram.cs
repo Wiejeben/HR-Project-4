6923cdb7735afa5fcab0743e26fd3a3888ba99d3
Maarten de Graaf <maarten@webtraders.nl>
2016-06-30 11:49:59 +0200

    Implement bar models

--- AndroidBicycleInfo/Services/Diagram.cs
+++ AndroidBicycleInfo/Services/Diagram.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace AndroidBicycleInfo
 {
@@ -11,21 +14,79 @@ namespace AndroidBicycleInfo
 		private string YLabel;
 		private PlotModel Model;
 
-		public Diagram(string title, string yLabel, string xLabel)
+		public Diagram(string title = "", string yLabel = "", string xLabel = "")
 		{
 			this.Title = title;
 			this.YLabel = yLabel;
 			this.XLabel = xLabel;
+
+			// Create Model instance
+			this.Model = new PlotModel
+			{
+				Title = this.Title,
+				LegendPlacement = LegendPlacement.Outside,
+				LegendPosition = LegendPosition.BottomCenter,
+				LegendOrientation = LegendOrientation.Horizontal,
+				LegendBorderThickness = 0
+			};
 		}
 
-		public PlotModel CreateBarModel(List<string> x, List<string> y)
+		public PlotModel CreateBarModel(Dictionary<string, int> data)
 		{
-			throw new NotImplementedException();
+			CategoryAxis yAxis = new CategoryAxis { Title = this.YLabel, Position = AxisPosition.Left, IsPanEnabled = false, IsZoomEnabled = false };
+			LinearAxis xAxis = new LinearAxis { Title = this.XLabel, Position = AxisPosition.Bottom, Minimum = 0, AbsoluteMinimum = 0 };
+
+			// Create bars
+			List<BarSeries> bars = new List<BarSeries>();
+			for (int i = 1; i < 5; i++)
+			{
+				bars.Add(new BarSeries { LabelPlacement = LabelPlacement.Inside, LabelFormatString = "{0}" });
+			}
+
+			// Parse values
+			foreach (KeyValuePair<string, int> entry in data)
+			{
+				yAxis.Labels.Add(entry.Key);
+				bars.First().Items.Add(new BarItem { Value = entry.Value });
+			}
+
+			bars.ForEach(bar => this.Model.Series.Add(bar));
+
+			// Add the axes to the model.
+			this.Model.Axes.Add(xAxis);
+			this.Model.Axes.Add(yAxis);
+
+			return this.Model;
 		}
 
-		public PlotModel CreateTwoBarModel(List<string> yLabels, Dictionary<string, int> barOne, Dictionary<string, int> barTwo)
+		public PlotModel CreateTwoBarModel(List<string> yLabels, string barOneName, List<int> barOne, string barTwoName, List<int> barTwo)
 		{
-			throw new NotImplementedException();
+
+			// Labels
+			CategoryAxis yAxis = new CategoryAxis { Title = this.YLabel, Position = AxisPosition.Left, IsZoomEnabled = false, IsPanEnabled = false };
+			// Values
+			LinearAxis xAxis = new LinearAxis { Title = this.XLabel, Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
+
+			// Bars
+			BarSeries firstBar = new BarSeries { Title = barOneName, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+			BarSeries secondBar = new BarSeries { Title = barTwoName, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
+
+			// Implement labels
+			yLabels.ForEach(value => yAxis.Labels.Add(value));
+
+			// Implement bar values
+			barOne.ForEach(value => firstBar.Items.Add(new BarItem { Value = value }));
+			barTwo.ForEach(value => secondBar.Items.Add(new BarItem { Value = value }));
+
+			// Add axises to model
+			this.Model.Axes.Add(yAxis);
+			this.Model.Axes.Add(xAxis);
+
+			// Combine bars and add to Model
+			this.Model.Series.Add(firstBar);
+			this.Model.Series.Add(secondBar);
+
+			return this.Model;
 		}
 
 		public PlotModel CreatePieModel(Dictionary<string, int> data)
