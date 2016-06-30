077a8536c096dfd9ddaf171a17d213ab398f2dd0
Maarten de Graaf <maarten@webtraders.nl>
2016-06-30 15:46:52 +0200

    Remove unused code

--- AndroidBicycleInfo/MainActivity.cs
+++ AndroidBicycleInfo/MainActivity.cs
@@ -5,38 +5,20 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android;
 
 namespace AndroidBicycleInfo
 {
     [Activity(Label = "AndroidBicycleInfo", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
-
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            //SetContentView(Resource.Layout.Main);
 
 			Database.Boot(this);
-
-			var db = Database.Load();
-
-			var result = db.Table<Brand>();
-
-			Toast.MakeText(this, "Start", ToastLength.Short).Show();
-			foreach (Brand entry in result)
-			{
-				Toast.MakeText(this, entry.name, ToastLength.Short).Show();
-			}	
         }
     }
 }
