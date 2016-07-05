aa832a0db95b31e77a15172af5bba8058598f3f2
Andy Bhadai <abhadai@gmail.com>
2016-07-05 15:19:25 +0200

    Added the OptionVisitor to the menu

--- Services/Menu.cs
+++ Services/Menu.cs
@@ -14,38 +14,18 @@ namespace AndroidBicycleInfo
 		public Menu(Activity activity)
 		{
             this.Activity = activity;
-            List<Button> ButtonList = new List<Button>();
+            Dictionary<Button, Type> ButtonDictionary = new Dictionary<Button, Type>();
 
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag1));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag2));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag3));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag4));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag5));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag6));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag7));
-            ButtonList.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag8));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag1), typeof(BikeLocationsActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag2), typeof(BikeContainersAndBikeTheftsActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag3), typeof(BikeContainerNeighborhoodsActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag4), typeof(BikeTheftColorsAndBrandsActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag5), typeof(BikeTheftsPerMonthActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag6), typeof(BikeAgendaActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag7), typeof(BikeTheftsPerNeighborhoodActivity));
+            ButtonDictionary.Add(Activity.FindViewById<Button>(Resource.Id.buttonVraag8), typeof(RouteCalculatorActivity));
 
-            this.InitializeMenu(ButtonList);
-
-            // Assign element
-            Button bikeLocationsActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag1);
-            Button bikeContainersAndBikeTheftsActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag2);
-            Button bikeContainerNeighborhoodsActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag3);
-            Button bikeTheftColorsAndBrandsActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag4);
-            Button bikeTheftsPerMonthActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag5);
-            Button bikeAgendaActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag6);
-            Button bikeTheftsPerNeighborhoodActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag7);
-            Button routeCalculatorActivity = Activity.FindViewById<Button>(Resource.Id.buttonVraag8);
-
-            // Assign events
-            bikeLocationsActivity.Click += delegate { this.LocationCheck(typeof(BikeLocationsActivity)); };
-			bikeContainersAndBikeTheftsActivity.Click += delegate { this.StartActivity(typeof(BikeContainersAndBikeTheftsActivity)); };
-			bikeContainerNeighborhoodsActivity.Click += delegate { this.StartActivity(typeof(BikeContainerNeighborhoodsActivity)); };
-			bikeTheftColorsAndBrandsActivity.Click += delegate { this.StartActivity(typeof(BikeTheftColorsAndBrandsActivity)); };
-			bikeTheftsPerMonthActivity.Click += delegate { this.StartActivity(typeof(BikeTheftsPerMonthActivity)); };
-			bikeAgendaActivity.Click += delegate { this.LocationCheck(typeof(BikeAgendaActivity)); };
-			bikeTheftsPerNeighborhoodActivity.Click += delegate { this.LocationCheck(typeof(BikeTheftsPerNeighborhoodActivity)); };
-			routeCalculatorActivity.Click += delegate { this.LocationCheck(typeof(RouteCalculatorActivity)); };
+            this.InitializeMenu(ButtonDictionary);
         }
 
 		private void LocationCheck(Type type)
@@ -60,13 +40,14 @@ namespace AndroidBicycleInfo
 			}
 		}
 
-        private void InitializeMenu(List<Button> ButtonList)
+        // Initialize the menu and add the click functions to buttons
+        private void InitializeMenu(Dictionary<Button, Type> ButtonDictionary)
         {
-            foreach(var button in ButtonList)
+            foreach(var entry in ButtonDictionary)
             {
-                Option<Button> ButtonOption = new Some<Button>(button);
-                ButtonOption.Visit<Button>(()=> { throw new Exception(""); }, 
-                    btn => {  throw new Exception(button.GetType().ToString()); } );
+                Option<Button> ButtonOption = new Some<Button>(entry.Key);
+                ButtonOption.Visit<Button>(()=> { throw new Exception("Not a button!"); }, 
+                    button => { button.Click += delegate { this.StartActivity(entry.Value); }; return button; });
             }
         }
 
